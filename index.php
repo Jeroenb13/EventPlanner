13e98441c7d93e9adf97b5e32c810266175b33e4
Jeroenb13 <jeroen.bakker@student.stenden.com>
2017-11-20 11:10:24 +0100

    Jquery toegevoegt aan de index

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <title>EventPlanner van de gemeente</title>
     <link rel="stylesheet" href="src/style/styleMain.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="src/scripts/main.js"></script>
 
 </head>
