b344beade815f09c277b5a0e2577021792330362
Jeroenb13 <jeroen.bakker@student.stenden.com>
2017-11-20 10:20:14 +0100

    Alles verwijdert en begonnen met php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jeroen
+ * Date: 20-11-2017
+ * Time: 10:18
+ */
--- src/app/components/main-page/main-page.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'alarmering-app',
-    templateUrl: './main-page.html'
-})
-export class MainPageComponent {
-    public navbarCollapsed: boolean = true;
-}
