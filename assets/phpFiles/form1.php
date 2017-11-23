89b5380f9483cad3b81bfba6c4246e559b5e57cb
Frans Orsel <theforsel@live.nl>
2017-11-23 12:15:44 +0100

    Added form to add stuffs

---
+++ assets/phpFiles/form1.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: forsel
+ * Date: 23-11-2017
+ * Time: 11:44
+ */
+
+?>
+
+<form id="inputForm" method="post" action="">
+    <p>Naam:</p>
+    <input type="text" name="nameInput">
+    <br/>
+    <p>Categorie</p>
+    <select name="inputCat">
+        <option value="Bierpompen">Bierpompen</option>
+        <option value="eetkramen">eet- en drankkramen</option>
+        <option value="Toiletten">Toiletten</option>
+        <option value="muntjesVerkoop">muntjes verkoopkramen</option>
+        <option value="Springkussens">Springkussens</option>
+        <option value="Attracties">Attracties</option>
+        <option value="muzikant">muzikant(single, duo, band)</option>
+        <option value="EHBO">EHBO stand</option>
+        <option value="techniek sectie">Techniek sectie</option>
+    </select>
+    <input type="submit" value="Toevoegen" name="submit">
+</form>
