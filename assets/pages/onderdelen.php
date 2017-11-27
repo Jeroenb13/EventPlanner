ad969860d230771f33d8f4ee3318ffcba10ee539
Yosh98 <Yosh98@gmx.com>
2017-11-27 14:06:17 +0100

    Onderdelen pagina, container + nav voor kaart en formulier

--- assets/pages/onderdelen.php
+++ assets/pages/onderdelen.php
@@ -8,3 +8,35 @@
             <li><a href="?page=onderdelen">Onderdelen</a></li>
         </ul>
     </div>
+
+    <div id="page-container">
+
+        <h1>Event Planner</h1>
+        <h2>Onderdelen</h2>
+
+        <div class="button buttonFloatLeft">
+            <a href="?page=formulier"><button>+ Nieuw Object</button></a>
+        </div>
+
+        <table id="onderdelenTabel">
+            <tr>
+                <th></th>
+                <th>Naam</th>
+                <th>Categorie</th>
+                <th></th>
+            </tr>
+            <tr>
+                <td>kleurtje</td>
+                <td>Podium1</td>
+                <td>Podia</td>
+                <td>Wijzigen | Bekijken | Verwijderen</td>
+            </tr>
+            <tr>
+                <td>kleurtje</td>
+                <td>Podium1</td>
+                <td>Podia</td>
+                <td>Wijzigen | Bekijken | Verwijderen</td>
+            </tr>
+        </table>
+
+    </div>
