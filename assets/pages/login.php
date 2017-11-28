6d00d0bd7f45bbcb74f6f9b097d63177663b631f
Jeroenb13 <jeroen.bakker@student.stenden.com>
2017-11-28 12:23:26 +0100

    Login functioneel gemaakt

--- assets/pages/login.php
+++ assets/pages/login.php
@@ -1,3 +1,45 @@
+<?php
+$error = '';
+$hash = "";
+$cLevel = 0;// Variabel om Errors op te slaan.
+if (isset($_POST['submit']))
+{
+
+    if (empty($_POST['uName']) || empty ($_POST['pWord']))
+    {
+        $error = "Gebruikersnaam en wachtwoord ongeldig";
+    } else
+    {
+        $un = $_POST['uName'];
+        $pw = $_POST['uName'] . $_POST['pWord'];
+
+// Gebruikerscode en ww pakken
+
+        $stmt = $db->prepare("SELECT 'cLevel' + 'pWord' FROM gebruiker WHERE uName = '{$un}';");
+
+        while($row = $stmt -> fetch())
+        {
+            $cLevel = $row['cLevel'];
+            $hash = $row['pWord'];
+        }
+
+        if(password_verify($pw, $hash))
+        {
+            $_SESSION['UserName'] = $un;
+            $_SESSION['CLevel'] = $cLevel;
+            $hash = "";
+            $pw = "";
+            echo "password valid";
+
+        }
+    }
+
+}
+if(password_verify("hoi", $hash)){
+    echo "<p>pass valid</p>";}
+else{"<p>Piet</p>";}
+?>
+
 <!--
 Inhoud:
     - Login form
@@ -8,3 +50,20 @@ Inhoud:
 -->
 <div id="container">
     <div id="upperbar"></div>
+
+    <h1>EventPlanner</h1>
+    <p>Log in om uw objecten aan te passen</p>
+
+
+    <form action="#" method="post">
+
+
+        <label>Gebruikersnaam</label>
+
+        <p> <input type="text" name="uName"></p>
+
+        <label>wachtwoord</label>:
+        <p>    <input type="password" name="pWord"></p>
+
+        <input type="submit" name="submit" value="inloggen">
+    </form>
