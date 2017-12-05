c99012b697259fa7afce23c71d723f9ce7f13ba2
Yosh98 <Yosh98@gmx.com>
2017-12-05 13:19:17 +0100

    - Login page -> H1 responsive fix - Map page -> Map objects div added, fixed with flexbox

--- assets/pages/kaart.php
+++ assets/pages/kaart.php
@@ -11,103 +11,105 @@
     </div>
 
     <h1>Event Planner</h1>
+    <h2>Kaart</h2>
 
-    <div class="paragraph">
-        <p>Maak een plattegrond voor het evenement dat
-            georganiseerd gaat worden door op
+    <div class="spacer-100"></div>
 
-    </div>
-
+    <!--
     <div id="legend">
         <h3>Legend</h3>
-    </div>
+    </div>-->
 
-    <div id="map">
-    <script>
-        var map;
-        function initMap() {
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: {lat: 52.783001, lng: 6.892503},
-                zoom: 17
-            });
+    <div id="mapAndObjectsContainer">
+        <div id="mapObjects"></div>
 
-            var imageBounds = {
-                north: 52.864812,
-                west: 6.812500,
-                south: 52.627515,
-                east: 7.078402
-            };
-
-
-
-            EmmenOverlay = new google.maps.GroundOverlay(
-                'https://www.lib.utexas.edu/maps/historical/newark_nj_1922.jpg', imageBounds);
-            EmmenOverlay.setMap(map);
-            EmmenOverlay.setOpacity(0.5);
-
-
-
-
-            var iconBase = 'assets/images/iconMap/';
-            var icons = {
-                energy: {
-                    name: 'Energy',
-                    icon: iconBase + 'bolt.png'
-                },
-                water: {
-                    name: 'Water',
-                    icon: iconBase + 'faucet.png'
-                }
-            };
-
-            var features = [
-                {
-                    position: new google.maps.LatLng(52.783162, 6.891798),
-                    type: 'energy'
-                }, {
-                    position: new google.maps.LatLng(52.783142, 6.891870),
-                    type: 'energy'
-                }, {
-                    position: new google.maps.LatLng(52.781969, 6.892700),
-                    type: 'energy'
-                }, {
-                    position: new google.maps.LatLng(52.784146, 6.892832),
-                    type: 'energy'
-                }, {
-                    position: new google.maps.LatLng(52.783102, 6.892630),
-                    type: 'energy'
-                }, {
-                    position: new google.maps.LatLng(52.783000, 6.892600),
-                    type: 'water'
-                }
-            ];
-
-            features.forEach(function (feature) {
-                var marker = new google.maps.Marker({
-                    position: feature.position,
-                    icon : icons[feature.type].icon,
-                    map: map
+        <div id="map">
+        <script>
+            var map;
+            function initMap() {
+                map = new google.maps.Map(document.getElementById('map'), {
+                    center: {lat: 52.783001, lng: 6.892503},
+                    zoom: 17
                 });
-            });
 
-            var legend = document.getElementById('legend');
-            for (var key in icons) {
-                var type = icons[key];
-                var name = type.name;
-                var icon = type.icon;
-                var div = document.createElement('div');
-                div.innerHTML = '<img src="' + icon + '"> ' + name;
-                legend.appendChild(div);
+                var imageBounds = {
+                    north: 52.864812,
+                    west: 6.812500,
+                    south: 52.627515,
+                    east: 7.078402
+                };
+
+
+
+                EmmenOverlay = new google.maps.GroundOverlay(
+                    'https://www.lib.utexas.edu/maps/historical/newark_nj_1922.jpg', imageBounds);
+                EmmenOverlay.setMap(map);
+                EmmenOverlay.setOpacity(0.5);
+
+
+
+
+                var iconBase = 'assets/images/iconMap/';
+                var icons = {
+                    energy: {
+                        name: 'Energy',
+                        icon: iconBase + 'bolt.png'
+                    },
+                    water: {
+                        name: 'Water',
+                        icon: iconBase + 'faucet.png'
+                    }
+                };
+
+                var features = [
+                    {
+                        position: new google.maps.LatLng(52.783162, 6.891798),
+                        type: 'energy'
+                    }, {
+                        position: new google.maps.LatLng(52.783142, 6.891870),
+                        type: 'energy'
+                    }, {
+                        position: new google.maps.LatLng(52.781969, 6.892700),
+                        type: 'energy'
+                    }, {
+                        position: new google.maps.LatLng(52.784146, 6.892832),
+                        type: 'energy'
+                    }, {
+                        position: new google.maps.LatLng(52.783102, 6.892630),
+                        type: 'energy'
+                    }, {
+                        position: new google.maps.LatLng(52.783000, 6.892600),
+                        type: 'water'
+                    }
+                ];
+
+                features.forEach(function (feature) {
+                    var marker = new google.maps.Marker({
+                        position: feature.position,
+                        icon : icons[feature.type].icon,
+                        map: map
+                    });
+                });
+
+                var legend = document.getElementById('legend');
+                for (var key in icons) {
+                    var type = icons[key];
+                    var name = type.name;
+                    var icon = type.icon;
+                    var div = document.createElement('div');
+                    div.innerHTML = '<img src="' + icon + '"> ' + name;
+                    legend.appendChild(div);
+                }
             }
-        }
 
-    </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBZHKVcs3VGPaxnuiQNFjOKL741xKu_vag&callback=initMap"
-            async defer></script>
+        </script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBZHKVcs3VGPaxnuiQNFjOKL741xKu_vag&callback=initMap"
+                async defer></script>
 
-    <script>
-        //AIzaSyBZHKVcs3VGPaxnuiQNFjOKL741xKu_vag api sleutel voor googlemaps
-    </script>
+        <script>
+            //AIzaSyBZHKVcs3VGPaxnuiQNFjOKL741xKu_vag api sleutel voor googlemaps
+        </script>
+        </div>
     </div>
 
 
