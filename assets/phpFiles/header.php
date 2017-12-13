126062265158064b967a3198144adda3836098ab
Yosh98 <Yosh98@gmx.com>
2017-12-13 12:44:10 +0100

    Fontello added, icons in login fields

--- assets/phpFiles/header.php
+++ assets/phpFiles/header.php
@@ -7,17 +7,15 @@ Inhoud:
 <html>
     <head>
         <title>Eventplanner - <?php echo $page;?></title>
-
         <!-- Stylesheets -->
         <link rel="stylesheet" href="assets/style/css/styleMain.css">
-        <link rel="stylesheet" href="assets/style/css/frontello.css">
+        <link rel="stylesheet" href="assets/style/css/fontello.css">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,400,600" rel="stylesheet">
-
         <!-- Scripts -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="assets/scripts/main.js"></script>
-
+        <script src="https://use.fontawesome.com/af3e5536fc.js"></script>
     </head>
 
     <!-- TODO: remove overlay after testing -->
