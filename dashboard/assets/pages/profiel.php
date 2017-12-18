9072ddab6503c12f944d023462b1c0ba80ca5693
Yosh98 <Yosh98@gmx.com>
2017-12-18 12:03:15 +0100

    Profiel bewerken pagina dashboard

--- dashboard/assets/pages/profiel.php
+++ dashboard/assets/pages/profiel.php
@@ -27,6 +27,18 @@
 
         <p class="p-margin-top">Op deze pagina kunt u uw eigen profiel wijzigen.</p>
 
+        <form method="post" action="#">
+          <hr>
+          <p>Wachtwoord</p>
+          <div class="inputGroup"><p>Nieuw wachtwoord:</p><input class="formInput" type="password"></div>
+          <div class="inputGroup"><p>Nieuw wachtwoord (nogmaals)</p><input class="formInput" type="password"></div>
+          <hr>
+          <p></p>
+          <input class="formInput" type="text">
+          <hr>
+          <input class="button" type="submit">
+
+        </form>
 
 
     </div>
