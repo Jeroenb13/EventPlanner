aa6208520319b31aba33c70ea22026b61da750cf
Jeroenb13 <jeroen.bakker@student.stenden.com>
2017-12-20 11:34:01 +0100

    Login functioneel gemaakt

--- assets/pages/kaart.php
+++ assets/pages/kaart.php
@@ -99,91 +99,32 @@
         </div>
 
         <div id="map">
-        <script>
-            var map;
-            function initMap() {
-                map = new google.maps.Map(document.getElementById('map'), {
-                    center: {lat: 52.783001, lng: 6.892503},
-                    zoom: 17
-                });
+            <style>
+                .embed-container
+                {position: relative; padding-bottom: 80%; height: 0; max-width: 100%;}
+                .embed-container iframe, .embed-container object, .embed-container
+                iframe{position: absolute; top: 0; left: 0; width: 100%; height: 100%;}
+                small{position: absolute; z-index: 40; bottom: 0; margin-bottom: -15px;}
+            </style>
+            <div class="embed-container">
+                <small>
+                    <a href="//emmen.maps.arcgis.com/apps/Embed/index.html?webmap=72b57ea630324946a63a1c7aa2b27ef2
+                       &amp;extent=6.4215,52.5441,7.6133,52.9832&home=true&zoom=true&scale=true&search=true&searchextent=true&legendlayers=true&disable_scroll=true&theme=light"
+                       style="color:#0000FF;text-align:left" target="_blank">Grotere kaart weergeven
+                    </a>
+                </small><br>
+                <iframe width="500" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
+                        title="Evenementenvergunning"
+                        src="//emmen.maps.arcgis.com/apps/Embed/index.html?webmap=72b57ea630324946a63a1c7aa2b27ef2&amp;extent=6.4215,52.5441,7.6133,52.9832&home=true&zoom=true&previewImage=false&scale=true&search=true&searchextent=true&legendlayers=true&disable_scroll=true&theme=light">
 
-                var imageBounds = {
-                    north: 52.864812,
-                    west: 6.812500,
-                    south: 52.627515,
-                    east: 7.078402
-                };
-
-
-                /*
-                EmmenOverlay = new google.maps.GroundOverlay(
-                    'https://www.lib.utexas.edu/maps/historical/newark_nj_1922.jpg', imageBounds);
-                EmmenOverlay.setMap(map);
-                EmmenOverlay.setOpacity(0.5);
-                */
-
-
-
-
-                var iconBase = 'assets/images/iconMap/';
-                var icons = {
-                    energy: {
-                        name: 'Energy',
-                        icon: iconBase + 'bolt.png'
-                    },
-                    water: {
-                        name: 'Water',
-                        icon: iconBase + 'faucet.png'
-                    }
-                };
-
-                var features = [
-                    {
-                        position: new google.maps.LatLng(52.783162, 6.891798),
-                        type: 'energy'
-                    }, {
-                        position: new google.maps.LatLng(52.783142, 6.891870),
-                        type: 'energy'
-                    }, {
-                        position: new google.maps.LatLng(52.781969, 6.892700),
-                        type: 'energy'
-                    }, {
-                        position: new google.maps.LatLng(52.784146, 6.892832),
-                        type: 'energy'
-                    }, {
-                        position: new google.maps.LatLng(52.783102, 6.892630),
-                        type: 'energy'
-                    }, {
-                        position: new google.maps.LatLng(52.783000, 6.892600),
-                        type: 'water'
-                    }
-                ];
-
-                features.forEach(function (feature) {
-                    var marker = new google.maps.Marker({
-                        position: feature.position,
-                        icon : icons[feature.type].icon,
-                        map: map
-                    });
-                });
-
-                var legend = document.getElementById('legend');
-                for (var key in icons) {
-                    var type = icons[key];
-                    var name = type.name;
-                    var icon = type.icon;
-                    var div = document.createElement('div');
-                    div.innerHTML = '<img src="' + icon + '"> ' + name;
-                    legend.appendChild(div);
-                }
-            }
-
-        </script>
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBZHKVcs3VGPaxnuiQNFjOKL741xKu_vag&callback=initMap"
-                async defer></script>
-
+                </iframe>
+            </div>
         <script>
             //AIzaSyBZHKVcs3VGPaxnuiQNFjOKL741xKu_vag api sleutel voor googlemaps
         </script>
         </div>
     </div>
+
+
+
+
