2304ba6264208fbabe39dd9344d0f2eca6be388d
Yosh98 <Yosh98@gmx.com>
2018-01-14 21:42:14 +0100

    - Chrome font-weight fixes

--- assets/phpFiles/header.php
+++ assets/phpFiles/header.php
@@ -11,7 +11,7 @@ Inhoud:
         <link rel="stylesheet" href="assets/style/css/styleMain.css">
         <link rel="stylesheet" href="assets/style/css/fontello.css">
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,400,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,600" rel="stylesheet">
         <!-- Scripts -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="assets/scripts/main.js"></script>
