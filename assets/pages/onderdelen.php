ae2dcb4c5d751d7a9df60a528e1db213b26e9fec
Yosh98 <Yosh98@gmx.com>
2018-01-16 10:32:22 +0100

    - User icon added in subpages - Dashboard links to login page if not logged in

--- assets/pages/onderdelen.php
+++ assets/pages/onderdelen.php
@@ -5,6 +5,10 @@
     <div id="nav">
         <ul>
           <li><a onclick="showNav()">☰</a></li>
+          <li><a id="loginDropdown-click" href="#" onclick="showLoginInfo()"><i class="icon-user nav-user-icon"></i></a>
+            <div id="loginDropdown"> <p id="IngelogdNaam">Jan Jansman</p>
+            <a class="login-dropdown-a">Profiel<i class="icon-pencil"></i></a> <a class="login-dropdown-a">Uitloggen<i class="icon-logout"></i></a> </div>
+          </li>
           <li><a href="?page=kaart">Kaart</a></li>
           <li><a href="?page=onderdelen">Onderdelen</a></li>
           <li><a href="?page=basisgegevens">Basisgegevens</a></li>
@@ -17,6 +21,14 @@
         <li><a href="?page=onderdelen">Onderdelen</a></li>
         <div class="nav-divider"></div>
         <li><a href="?page=kaart">Kaart</a></li>
+        <div class="nav-divider"></div>
+
+        <li class="nav-overlay-user-li">
+          <i class="icon-user nav-overlay-icon-user"></i><p id="nav-overlay-ingelogdNaam">Jan Jansman</p>
+          <a class="nav-overlay-login-a">Profiel<i class="icon-pencil"></i></a>
+          <a class="nav-overlay-login-a">Uitloggen<i class="icon-logout"></i></a>
+        </li>
+
       </ul>
     </div>
 
