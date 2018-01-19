366ebe0754f0e89f0b4f105717e07b0d3bc034c2
LumaLux <jasper.folkers@student.stenden.com>
2018-01-19 10:29:13 +0100

    Kaart + accountMaken updated

--- assets/pages/kaart.php
+++ assets/pages/kaart.php
@@ -1,37 +1,4 @@
 <!-- MAP page-->
-<div id="container-plus">
-    <div id="upperbar"></div>
-
-    <!-- Navigation -->
-    <div id="nav">
-        <ul>
-          <li><a onclick="showNav()">☰</a></li>
-          <li><a id="loginDropdown-click" href="#" onclick="showLoginInfo()"><i class="icon-user nav-user-icon"></i></a>
-            <div id="loginDropdown"> <p id="IngelogdNaam">Jan Jansman</p>
-            <a class="login-dropdown-a">Profiel<i class="icon-pencil"></i></a> <a class="login-dropdown-a">Uitloggen<i class="icon-logout"></i></a> </div>
-          </li>
-          <li><a href="?page=kaart">Kaart</a></li>
-          <li><a href="?page=onderdelen">Onderdelen</a></li>
-          <li><a href="?page=basisgegevens">Basisgegevens</a></li>
-        </ul>
-    </div>
-    <div id="nav-overlay">
-      <ul>
-        <li><a href="?page=basisgegevens">Basisgegevens</a></li>
-        <div class="nav-divider"></div>
-        <li><a href="?page=onderdelen">Onderdelen</a></li>
-        <div class="nav-divider"></div>
-        <li><a href="?page=kaart">Kaart</a></li>
-        <div class="nav-divider"></div>
-
-        <li class="nav-overlay-user-li">
-          <i class="icon-user nav-overlay-icon-user"></i><p id="nav-overlay-ingelogdNaam">Jan Jansman</p>
-          <a class="nav-overlay-login-a">Profiel<i class="icon-pencil"></i></a>
-          <a class="nav-overlay-login-a">Uitloggen<i class="icon-logout"></i></a>
-        </li>
-
-      </ul>
-    </div>
 
     <h1>Event Planner</h1>
     <h2>Kaart</h2>
