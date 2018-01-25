422f959277987e6255b94b3a2921ea82bb4e6fb6
LumaLux <jasper.folkers@student.stenden.com>
2018-01-25 13:08:53 +0100

    fixed database files

--- assets/phpFiles/database.php
+++ assets/phpFiles/database.php
@@ -1,18 +1,8 @@
 <?php
-$serverName = $_SERVER['SERVER_NAME'];
-if($serverName == "inf2j.aryoco.nl") {
-    $dsn = "mysql:dbname=aryocon_inf2j;host=localhost;port=3306;charset=UTF8";
-    $username="aryocon_inf2j";
-    $password="geefbier";
-}else if($serverName == "eventplanner.joeymariah.me") {
-    $dsn = "mysql:dbname=joeynlxj_Eventplanner;host=localhost;port=3306;charset=UTF8";
-    $username="joeynlxj_Eventplanner";
-    $password="dinges22";
-}else {
+
     $dsn = "mysql:dbname=aryocon_inf2j;host=localhost;port=3306;charset=UTF8";
     $username="root";
     $password="";
-}
 
 
 try {
